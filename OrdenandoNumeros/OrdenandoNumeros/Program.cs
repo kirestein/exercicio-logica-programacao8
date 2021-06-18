e63a39d79305ff10bec86e58a5d8192fb191d640
= <=>
2021-06-18 19:22:04 -0300

    Fazendo exercício 8 - Ordenando números

---
+++ OrdenandoNumeros/OrdenandoNumeros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OrdenandoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int numero;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Informe o {i}º número");
+                numero = int.Parse(Console.ReadLine());
+                array[i] = numero;
+            }
+
+            Console.WriteLine("Array ordenado");
+
+            Array.Sort(array);
+
+            foreach (int value in array) Console.WriteLine("[" + value + "]");
+
+            Console.ReadLine();
+
+        }
+    }
+}
